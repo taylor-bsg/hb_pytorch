ec3306bf4d561dde7afe5a0cd9dd2289f18db0b7
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-12-11 04:41:49 -0800

    Updated exmaple conv dimensions

--- conv2d_example.py
+++ conv2d_example.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 
 torch.manual_seed(0)
 
-m = nn.Conv2d(16, 33, 3, stride=2)
-input = torch.randn(20, 16, 50, 100)
+m = nn.Conv2d(16, 8, 8, stride=2)
+input = torch.randn(4, 16, 1024, 1024)
 output = m(input)
 
 print(output.sum())
